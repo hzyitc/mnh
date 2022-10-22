467f06b8f8c067cd0012a30da04f33350a143f53
hzy <hzyitc@outlook.com>
2022-10-22 16:22:29 +0800

    Use `github.com/huin/goupnp`

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.16
 
 require (
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
+	github.com/huin/goupnp v0.0.0-20221002135549-d8bd5d2d52c1
 	github.com/hzyitc/netutils v0.1.0
 	github.com/libp2p/go-reuseport v0.0.2
 	github.com/spf13/cobra v1.1.3
-	gitlab.com/NebulousLabs/go-upnp v0.0.0-20211002182029-11da932010b6
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
--- routerForward/upnp.go
+++ routerForward/upnp.go
@@ -2,37 +2,97 @@ package routerForward
 
 import (
 	"context"
+	"errors"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"gitlab.com/NebulousLabs/go-upnp"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/huin/goupnp"
+	"github.com/huin/goupnp/dcps/internetgateway2"
 )
 
 type UPNP struct {
-	udp  bool
-	port uint16
+	protocol string
+	port     uint16
 
-	igd *upnp.IGD
+	client interface {
+		GetServiceClient() *goupnp.ServiceClient
+
+		AddPortMappingCtx(
+			ctx context.Context,
+			NewRemoteHost string,
+			NewExternalPort uint16,
+			NewProtocol string,
+			NewInternalPort uint16,
+			NewInternalClient string,
+			NewEnabled bool,
+			NewPortMappingDescription string,
+			NewLeaseDuration uint32,
+		) (err error)
+
+		DeletePortMappingCtx(
+			ctx context.Context,
+			NewRemoteHost string,
+			NewExternalPort uint16,
+			NewProtocol string,
+		) (err error)
+	}
 }
 
-func newUPNP(udp bool, port int) (Interface, error) {
+func newUPNP(protocol string, port int) (Interface, error) {
+	u := &UPNP{
+		protocol: protocol,
+		port:     uint16(port),
+	}
+
 	ctx, done := signal.NotifyContext(context.TODO(), os.Interrupt, syscall.SIGTERM)
 	defer done()
 
-	igd, err := upnp.DiscoverCtx(ctx)
-	if err != nil {
+	tasks, _ := errgroup.WithContext(ctx)
+
+	// Request each type of client in parallel, and return what is found.
+	var ip1Clients []*internetgateway2.WANIPConnection1
+	tasks.Go(func() error {
+		var err error
+		ip1Clients, _, err = internetgateway2.NewWANIPConnection1ClientsCtx(ctx)
+		return err
+	})
+
+	var ip2Clients []*internetgateway2.WANIPConnection2
+	tasks.Go(func() error {
+		var err error
+		ip2Clients, _, err = internetgateway2.NewWANIPConnection2ClientsCtx(ctx)
+		return err
+	})
+
+	var ppp1Clients []*internetgateway2.WANPPPConnection1
+	tasks.Go(func() error {
+		var err error
+		ppp1Clients, _, err = internetgateway2.NewWANPPPConnection1ClientsCtx(ctx)
+		return err
+	})
+
+	if err := tasks.Wait(); err != nil {
 		return nil, err
 	}
 
-	u := &UPNP{
-		udp,
-		uint16(port),
-
-		igd,
+	// Trivial handling for where we find exactly one device to talk to, you
+	// might want to provide more flexible handling than this if multiple
+	// devices are found.
+	switch {
+	case len(ip2Clients) == 1:
+		u.client = ip2Clients[0]
+	case len(ip1Clients) == 1:
+		u.client = ip1Clients[0]
+	case len(ppp1Clients) == 1:
+		u.client = ppp1Clients[0]
+	default:
+		return nil, errors.New("multiple or no services found")
 	}
 
-	err = u.Redo()
+	err := u.Redo()
 	if err != nil {
 		return nil, err
 	}
@@ -41,21 +101,32 @@ func newUPNP(udp bool, port int) (Interface, error) {
 }
 
 func NewTCPForward_UPNP(port int) (Interface, error) {
-	return newUPNP(false, port)
+	return newUPNP("TCP", port)
 }
 
 func NewUDPForward_UPNP(port int) (Interface, error) {
-	return newUPNP(true, port)
+	return newUPNP("UDP", port)
 }
 
 func (u *UPNP) Redo() error {
-	if u.udp {
-		return u.igd.ForwardUDP(u.port, "mnh")
-	} else {
-		return u.igd.ForwardTCP(u.port, "mnh")
-	}
+	return u.client.AddPortMappingCtx(
+		context.Background(),
+		"",
+		u.port,
+		u.protocol,
+		u.port,
+		u.client.GetServiceClient().LocalAddr().String(),
+		true,
+		"mnh",
+		0,
+	)
 }
 
 func (u *UPNP) Close() {
-	u.igd.Clear(u.port)
+	u.client.DeletePortMappingCtx(
+		context.Background(),
+		"",
+		u.port,
+		u.protocol,
+	)
 }
